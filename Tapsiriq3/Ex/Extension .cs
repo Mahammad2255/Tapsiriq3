543365e86fb17c0fa4896311f0723ee3a45881a7
Mahammad2255 <[mahammadei@code.edu.az]>
2021-12-19 11:28:17 -0800

    Add project files.

---
+++ Tapsiriq3/Ex/Extension .cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tapsiriq3.Ex
+{
+    public static class Extension
+    {
+        
+        public static bool Yoxla(this string word, string axtar)
+        {
+            string n = axtar.ToLower();
+            string m = word.ToLower();
+            //Contains metodunu arasdirib yazdim, basqa cur de yazmaga calisdim ancaq "Hello World"
+            //yazdigdan sonra Hell yoxlayanda true, Hellt meselen yoxlayanda da true verirdi, ona gore bu usulla yazdim
+            if(m.Contains(n) == true)
+            {
+                return true;
+            }
+            return false;
+            //string[] arr = word.Split(' ');
+            //StringBuilder sb2 = new StringBuilder(axtar);
+            //StringBuilder sb = new StringBuilder(word);
+
+            //for (int i = 0, b=0; i < arr.Length; i++, b++)
+            //{
+
+            //    if(sb[i] == sb2[b])
+            //    {
+            //        return true;
+            //    }
+            //}
+            //string[] arr = word.Split(' ');
+            //StringBuilder sb2 = new StringBuilder(axtar);
+
+            //foreach (var item in arr)
+            //{
+
+            //   if(item == axtar)
+            //    {
+            //        return true;
+            //    }
+            //    int i = 0;
+            //    int b = 0;
+
+
+
+            //---------------------------------------
+            //    if (item[i] == sb2[b])
+            //    {
+            //        i++;
+            //        b++;
+            //        for (int k = 0, j=0; k < sb2.Length; j++,k++)
+            //        {
+            //            if(item[j] == sb2[k])
+            //            {
+            //                return true;
+
+            //            }
+            //            return false;
+            //        }
+            //        //return true;
+            //    }
+            //}
+
+            
+        }
+    }
+}
---
+++ Tapsiriq3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Tapsiriq3.Ex;
+
+namespace Tapsiriq3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string soz = "Hello World";
+            Console.WriteLine(soz.Yoxla("rld"));
+        }
+      
+    }
+}
